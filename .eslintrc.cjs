ae16213d0759ec78ac493830e082e88e15ef16dc
scott.wallace.personal <scott.wallace.personal@gmail.com>
2025-08-28 14:46:49 +0000

    fux attempt 1

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+// wswallace-62/buz-tracker2/Buz-Tracker2-Github-errors/.eslintrc.cjs
 // .eslintrc.cjs
 module.exports = {
   parser: '@typescript-eslint/parser',
@@ -6,6 +7,9 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
   ],
+  // Ignore the functions directory, as it has its own linting process.
+  // Also, ignore this config file itself.
+  ignorePatterns: ['functions/**/*', '.eslintrc.cjs'],
   rules: {
     // CI is failing on "Unexpected any" and warnings are treated as errors.
     '@typescript-eslint/no-explicit-any': 'off',
